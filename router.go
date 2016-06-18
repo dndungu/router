f253127af9185c10f7efc36b96522fe5cb2e41da
Grace Kiburi <grace.kiburi@gmail.com>
2016-06-17 18:08:57 -0700

    renamed unexported methods to start with lowercase

--- router.go
+++ router.go
@@ -15,19 +15,19 @@ type Router struct {
 }
 
 func New(handler Handler) *Router {
-	root := NewNode()
+	root := newNode()
 	return &Router{tree: root, handler: handler}
 }
 
 func (r *Router) Add(method, path string, handlers ...Handler) {
-	r.tree.Insert(method, path, handlers...)
+	r.tree.insert(method, path, handlers...)
 }
 
 func (r *Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	params := url.Values{}
 	context.Set(req, "params", params)
 	keys := strings.Split(req.URL.Path, "/")[1:]
-	node, _ := r.tree.Search(strings.Join(keys, "/"), params)
+	node, _ := r.tree.search(strings.Join(keys, "/"), params)
 	handlers := node.handlers[req.Method]
 	if handlers == nil {
 		r.handler(w, req)
